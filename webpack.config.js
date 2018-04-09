60edc8f564365f612dc4c7c6db389d2e41d1ed91
Anthony Banville <abanvill@brunhilde.local>
2018-04-09 11:52:21 +0200

    working configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
+const path = require('path')
+
 module.exports = {
+  entry: [ 'babel-polyfill', './src/index.js' ],
   mode: 'development',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
-        options: {
-          // eslint options (if necessary)
-        }
+        loader: 'eslint-loader',
+        options: {} 
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
     ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
 }
